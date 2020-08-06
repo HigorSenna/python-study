c7274f2bdb8308d04a6dba2319ade2c7713bec30
Higor Senna <higorrebjfmg@gmail.com>
2020-08-06 19:46:06 -0300

    organizando pastas

---
+++ guppe/loops/breaks.py
@@ -0,0 +1,10 @@
+for numero in range(1, 11):
+    if numero == 6:
+        break
+    else:
+        print(numero)
+
+while True:
+    comando = input("Digite 'sair' para sair: ")
+    if comando == 'sair':
+        break
---
+++ guppe/loops/loop_while.py
@@ -0,0 +1,22 @@
+"""
+Loop while
+
+Forma geral:
+
+while expressao_booleana:
+    //implementacao
+"""
+
+
+numero = 1
+
+while numero <= 10:
+    print(numero)
+    numero += 1
+
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input('Deseja parar? ')
+
+
