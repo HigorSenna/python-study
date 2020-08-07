ece8603c588b9922f4c57a5ba4f6f743322b4cba
s2it_hchaves <s2it_hchaves@uolinc.com>
2020-08-07 08:40:45 -0300

    iniciando com listas

---
+++ guppe/listas.py
@@ -0,0 +1,20 @@
+"""
+Listas Python
+
+Listas em python funcionam como arrays, com a diferença
+de serem dinamicos e tambem de podermos colocar QUALQUER tipo de dado
+
+OBS:
+- Dinâmico: Não possuem tamanho fixo; Ou seja podemos criar a lista
+e simplesmente ir adicionando elementos; (enquanto estiver memória disponível)
+
+- Qualquer tipo de dado: Nao possuem tipo de dado fixo, ou seja, podemos colocar qualquer tipo de dado
+na mesma lista.
+"""
+
+type([])
+
+lista1 = [1, 2, 3, 4, 5]
+lista2 = ['G', 's', 'L']
+lista3 = []
+lista4 = list(range(11))
