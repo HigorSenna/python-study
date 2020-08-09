6e2fcad60edba291985e62dc3ad027e417bf4b09
Higor Senna <higorrebjfmg@gmail.com>
2020-08-09 18:06:19 -0300

    set_comprehensions

---
+++ guppe/comprehensions/set_comprehensions.py
@@ -0,0 +1,11 @@
+"""
+Set Comprehensions
+"""
+
+# Exemplos
+
+numeros = {num for num in range(1, 7)}
+print(numeros)
+
+letras = {letra for letra in 'Gerando um set de string'}
+print(letras)  # Nao tem repetição e nem ordenação
