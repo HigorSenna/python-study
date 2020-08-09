ca9354e6ff4c3172670723440c9db76c9d54b250
Higor Senna <higorrebjfmg@gmail.com>
2020-08-09 10:39:25 -0300

    funcao com parametro

---
+++ guppe/funcoes/funcoes_com_parametro.py
@@ -0,0 +1,34 @@
+"""
+Funcoes com parâmetro
+"""
+
+def obter_nome_completo(nome, sobrenome):
+    return f'Seu nome completo é {nome} {sobrenome}'
+
+
+print(obter_nome_completo('Angelina', 'Jolie'))
+
+
+# Argumentos nomeados
+
+print(obter_nome_completo(sobrenome='Jolie', nome='Angelia'))
+print(obter_nome_completo(nome='Angelia', sobrenome='Jolie'))
+
+
+def quadrado(numero):
+    return numero ** 2
+
+
+print(quadrado(7))
+print(quadrado(8))
+print(quadrado(2))
+
+
+def soma(a, b):
+    return a + b
+
+
+print(soma(1, 3))
+
+
+
