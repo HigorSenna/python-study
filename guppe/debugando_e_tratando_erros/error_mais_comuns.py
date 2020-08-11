68f1449eb74bb554aa76ebb06d5ca11e5bc0b5f5
Higor Senna <higorrebjfmg@gmail.com>
2020-08-11 18:46:24 -0300

    erros_mais_comuns

---
+++ guppe/debugando_e_tratando_erros/error_mais_comuns.py
@@ -0,0 +1,23 @@
+"""
+Erros mais comuns em Python
+
+
+SyntaxError -> ocorre quando o python encontra um erro de sintaxe, ou seja, voce escreveu algo
+que o python nao reconhece como parte da linguagem
+
+NameError -> ocorre quando uma variável ou função nao foi definida.
+
+TypeError -> Ocorre quando uma função/operação é aplicada a um tipo errado.
+
+IndexError -> Ocorre quando tentamos acessar um elemento em uma lista ou outro tipo de dado indexado
+utilizando um índice inválido
+
+ValueError -> Ocorre quando uma função built-in recebe um argumento com tipo correto mas valor
+inapropriado
+
+KeyError -> Ocorre quando tentamos acessar um dicionario com uma chave que nao existe
+
+AttributeError -> ocorre quando uma variavel nao tem um atributo/função
+
+IdentationError -> Ocorre quando nao respeitamos a indentação do python que é de 4 ESPAÇOS
+"""
